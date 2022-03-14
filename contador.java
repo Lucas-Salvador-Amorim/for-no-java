b4d9bd426985bb8c671f4f18f0e51323977f34f1
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-14 17:38:43 +0000

    Exercicio  contador

---
+++ contador.java
@@ -0,0 +1,10 @@
+public class contador{
+    public static void main(String[] args){
+        int numero = 150;
+        for(int conta = 0; conta <=150; conta++){
+            System.out.println("Numero atual: " + numero);
+            numero+=1;
+        }
+    }
+
+}
